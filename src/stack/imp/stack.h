3abc305a1e2e4f5b9fbc75b05086d297e87045bb
Ido Tauman <ido032009@gmail.com>
2026-05-06 01:14:45 +0300

    finished stack implementation

--- src/stack/imp/stack.h
+++ src/stack/imp/stack.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../../arraylist/imp/arraylist.h"
+#include <stdbool.h>
 
 /**
  * In this library, a Stack is a functional alias for an ArrayList.
@@ -8,9 +9,9 @@
  */
 typedef ArrayList Stack;
 
-Stack* mavnat_stack_init(Arena *arena, size_t initial_capacity);
-void   mavnat_stack_push(Stack *stack, void *item);
-void*  mavnat_stack_pop(Stack *stack);
-void*  mavnat_stack_peek(const Stack *stack);
-bool   mavnat_stack_is_empty(const Stack *stack);
-size_t mavnat_stack_size(const Stack *stack);
+static inline Stack* mavnat_stack_init(Arena *arena, size_t initial_capacity);
+static inline void   mavnat_stack_push(Stack *stack, void *item);
+static inline void*  mavnat_stack_pop(Stack *stack);
+static inline void*  mavnat_stack_peek(const Stack *stack);
+static inline bool   mavnat_stack_is_empty(const Stack *stack);
+static inline size_t mavnat_stack_size(const Stack *stack);

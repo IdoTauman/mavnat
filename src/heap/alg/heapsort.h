ed22edff15b6e62130043eac0db61ca065b1e37c
Ido Tauman <ido032009@gmail.com>
2026-05-06 02:38:42 +0300

    moved implementation to .c file

--- src/heap/alg/heapsort.h
+++ src/heap/alg/heapsort.h
@@ -9,20 +9,4 @@
  * Time complexity: O(nlogn)
  * Space complexity: O(1)
  */
-void mavnat_heapsort(ArrayList *list, mavnat_compare_fn cmp) {
-    size_t n = list->size;
-    if (mavnat_unlikely(n <= 1)) return;
-
-    int flipped_cmp(const void *a, const void *b) {
-        return cmp(b, a);
-    }
-
-    mavnat_heapify(list, flipped_cmp);
-    void **data = list->data;
-    // reverse iteration to save cmp instructions
-    for (size_t i = n; i-- > 0;) {
-        data[i] = mavnat_heap_pop((Heap*)list, flipped_cmp);
-    }
-
-    list->size = n;
-}
+void mavnat_heapsort(ArrayList *list, mavnat_compare_fn cmp);
